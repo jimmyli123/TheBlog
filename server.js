cb80ecab4888f2d689165e338cc7832e5b273a43
Jimmy Li <lijjimmy@gmail.com>
2021-04-21 23:11:02 -0400

    Created post and get methods

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 })
 .catch(error => console.log('There is an error with Mongo Client.'))
 
-app.set('view engine', 'ejs')
-
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -29,7 +27,18 @@ app.get('/', async (req, response) => {
 })
 
 app.post('/addToBlog', (request,response) => {
+    console.log(request.body)
+    let timestamp = Date.now()
+    let date_posted = new Date()
+    date_posted = date_posted + date_posted.getTimezoneOffset() * 6000
     
+    db.collection('jimmyblog').insertOne({blogPost : request.body.blogItem, postDate: date_posted.substr()})
+    .then(result=> {
+        console.log(`Added ${result}`)
+        response.redirect('/')
+    })
+    .catch(error => console.log(error))
+
 })
 
 app.listen(process.env.PORT || PORT, ()=> {
